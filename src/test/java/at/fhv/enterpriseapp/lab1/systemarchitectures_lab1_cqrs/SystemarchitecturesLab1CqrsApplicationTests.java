143160a457a492d01568cb0ba5ff136921361221
bianca482 <80405628+bianca482@users.noreply.github.com>
2021-10-27 07:48:21 +0200

    Project setup

---
+++ src/test/java/at/fhv/enterpriseapp/lab1/systemarchitectures_lab1_cqrs/SystemarchitecturesLab1CqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemarchitecturesLab1CqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
