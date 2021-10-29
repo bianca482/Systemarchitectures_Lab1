d1cf0f9766a3967c7b3cc9eac0b49c1ffe636748
Ilona <iwi>
2021-10-30 01:02:32 +0200

    fixRandomIDCreator

--- src/main/java/at/fhv/enterpriseapp/lab1/systemarchitectures_lab1_cqrs/application/implementation/RandomIDCreatorImpl.java
+++ src/main/java/at/fhv/enterpriseapp/lab1/systemarchitectures_lab1_cqrs/application/implementation/RandomIDCreatorImpl.java
@@ -2,13 +2,12 @@ package at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs.application.impl
 
 import at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs.application.RandomIDCreator;
 
-import java.math.BigInteger;
 import java.util.UUID;
 
 public class RandomIDCreatorImpl implements RandomIDCreator {
 
     @Override
     public String generateId() {
-        return new BigInteger(UUID.randomUUID().toString(), 10).toString();
+        return UUID.randomUUID().toString();
     }
 }
