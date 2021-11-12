c8fd00e9dd3149a85def70caf7a52ebdd8dd2349
Ilona <iwi>
2021-11-12 18:28:08 +0100

    added exceptionhandling in WriteRestController and command.html

--- src/main/java/at/fhv/enterpriseapp/lab1/systemarchitectures_lab1_cqrs/writeside/WriteRestController.java
+++ src/main/java/at/fhv/enterpriseapp/lab1/systemarchitectures_lab1_cqrs/writeside/WriteRestController.java
@@ -24,17 +24,21 @@ public class WriteRestController {
 
     @PostMapping(value = "/book", produces = "application/json")
     public String saveBooking(@RequestParam("checkInDate") String checkInDateStr,
-                            @RequestParam("checkOutDate") String checkOutDateStr,
-                            @RequestParam("roomNr") String roomNrStr,
-                            @RequestParam("guestId") String guestIdStr
-    ) throws InvalidTimeRangeException, RoomOccupiedException {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDateTime checkInDate = LocalDate.parse(checkInDateStr, formatter).atTime(0, 0);
-        LocalDateTime checkOutDate = LocalDate.parse(checkOutDateStr, formatter).atTime(0, 0);
-        int roomNr = Integer.parseInt(roomNrStr);
+                              @RequestParam("checkOutDate") String checkOutDateStr,
+                              @RequestParam("roomNr") String roomNrStr,
+                              @RequestParam("guestId") String guestIdStr
+    ) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDateTime checkInDate = LocalDate.parse(checkInDateStr, formatter).atTime(0, 0);
+            LocalDateTime checkOutDate = LocalDate.parse(checkOutDateStr, formatter).atTime(0, 0);
+            int roomNr = Integer.parseInt(roomNrStr);
 
-        bookingWriteService.applyBookRoomCommand(new BookRoomCommand(checkInDate, checkOutDate, new RoomNr(roomNr), new GuestId(guestIdStr)));
-        return "{'status':'ok'}";
+            bookingWriteService.applyBookRoomCommand(new BookRoomCommand(checkInDate, checkOutDate, new RoomNr(roomNr), new GuestId(guestIdStr)));
+            return "{\"status\":\"ok\"}";
+        } catch (InvalidTimeRangeException | RoomOccupiedException ex) {
+            return "{\"status\":\"failed\", \"info\":\"" + ex.getClass().getSimpleName() + "\"}";
+        }
     }
 
     @PostMapping(value = "/cancel")
@@ -44,19 +48,5 @@ public class WriteRestController {
         bookingWriteService.applyCancelRoomCommand(new CancelRoomCommand(new ReservationNr(reservationNrStr)));
         return "{'status':'ok'}";
     }
-
-//  might be useful later
-//    @RequestMapping(value = "/book",
-//            method = RequestMethod.POST,
-//            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-//    public void createRole(@RequestBody MultiValueMap<String, String> formData) {
-//        String check_in = formData.getFirst("check_in");
-//        String check_out = formData.getFirst("check_out");
-//        String room_nr = formData.getFirst("room_nr");
-//        String guest_id = formData.getFirst("guest_id");
-//        // your code goes here
-//    }
-
-
 }
 
