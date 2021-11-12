c9ae64b27ce26ec3c000f1a18be08de92aff3b9b
bianca482 <80405628+bianca482@users.noreply.github.com>
2021-11-12 10:14:23 +0100

    GetMapping für GetBookingsInTimeRange + Anpassung query.html

--- src/main/java/at/fhv/enterpriseapp/lab1/systemarchitectures_lab1_cqrs/readside/ReadRestController.java
+++ src/main/java/at/fhv/enterpriseapp/lab1/systemarchitectures_lab1_cqrs/readside/ReadRestController.java
@@ -2,10 +2,17 @@ package at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs.readside;
 
 import at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs.eventside.domain.events.RoomBookedEvent;
 import at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs.eventside.domain.events.RoomCancelledEvent;
+import at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs.eventside.domain.model.Booking;
 import at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs.readside.infrastructure.BookingReadRepository;
+import at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs.readside.queries.GetBookingsInTimeRangeQuery;
+import at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs.readside.service.BookingReadService;
+import at.fhv.enterpriseapp.lab1.systemarchitectures_lab1_cqrs.readside.service.BookingReadServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -13,6 +20,9 @@ public class ReadRestController {
 
     @Autowired
     private BookingReadRepository readRepository;
+
+    @Autowired
+    private BookingReadServiceImpl readService;
 
     @PostMapping(value = "/event/booked", consumes = "application/json", produces = "application/json")
     public boolean subscribe(@RequestBody RoomBookedEvent bookedEvent) {
@@ -40,10 +50,16 @@ public class ReadRestController {
 //      query.html fertig machen --> JSON Daten holen und mittels javascript anzeigen
 //    }
 
-//    @GetMapping(value = "/getBookingInTimeRange", produces = "application/json")
-//      TODO
-//      query.html fertig machen --> JSON Daten holen und mittels javascript anzeigen
-//    }
+    @GetMapping(value = "/getBookingInTimeRange", produces = "application/json")
+    public List<Booking> getBookingsInTimeRange(@RequestParam("check_in2") String checkInDateStr,
+                                                @RequestParam("check_out2") String checkOutDateStr) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime checkInDate = LocalDate.parse(checkInDateStr, formatter).atTime(0, 0);
+        LocalDateTime checkOutDate = LocalDate.parse(checkOutDateStr, formatter).atTime(0, 0);
+
+        List<Booking> bookingsInTimeRange = readService.handleQuery(new GetBookingsInTimeRangeQuery(checkInDate, checkOutDate));
+        return bookingsInTimeRange;
+    }
 
 }
 
